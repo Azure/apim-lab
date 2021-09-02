d69cd26f01c36ddafd7b4cf33645a963f157e500
Fernando Mejia <66635444+ferantoMSFT@users.noreply.github.com>
2021-09-02 17:28:10 +0000

    updating setup.sh Implementing github codespaces for inner-loop using github pages, ruby #3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,7 +7,8 @@ apt-get install -y \
   jq \
   sudo \
   zsh \
-  icu-devtools
+  icu-devtools \
+  tar
 
 # setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -16,8 +17,8 @@ cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
-# setup and install ruby
-apt install curl g++, gcc, autoconf, automake, bison, libc6-dev, libffi-dev, libgdbm-dev, libncurses5-dev, libsqlite3-dev, libtool, libyaml-dev, make, pkg-config, sqlite3, zlib1g-dev, libgmp-dev, libreadline-dev, libssl-dev
+# setup and install ruby using rvm 
+apt-get install -y g++ gcc autoconf automake bison libc6-dev libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool libyaml-dev make pkg-config sqlite3 zlib1g-dev libgmp-dev libreadline-dev libssl-dev
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
