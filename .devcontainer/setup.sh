05af3629ec0f2acefbc8c6e7f1cf4a40c0289408
Fernando Mejia <66635444+ferantoMSFT@users.noreply.github.com>
2021-09-02 17:11:37 +0000

    testing new ruby installs #3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -22,7 +22,10 @@ sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
 # setup and install ruby
-sudo apt-get install -y ruby-full build-essential zlib1g-dev
-
-# setup and install jekyll
-gem install jekyll bundler
+sudo apt update
+sudo apt install curl g++, gcc, autoconf, automake, bison, libc6-dev, libffi-dev, libgdbm-dev, libncurses5-dev, libsqlite3-dev, libtool, libyaml-dev, make, pkg-config, sqlite3, zlib1g-dev, libgmp-dev, libreadline-dev, libssl-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.7.4
+rvm use 2.7.4 --default
