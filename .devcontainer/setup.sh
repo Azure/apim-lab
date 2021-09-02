59d84765760c0a63570c4ed7ad7c81828d746e85
Fernando Mejia <66635444+ferantoMSFT@users.noreply.github.com>
2021-09-02 18:28:55 +0000

    update Implementing github codespaces for inner-loop using github pages, ruby #3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -17,9 +17,4 @@ sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
 # setup and install ruby using rvm 
-apt-get install -y tar g++ gcc autoconf automake bison libc6-dev libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool libyaml-dev make pkg-config sqlite3 zlib1g-dev libgmp-dev libreadline-dev libssl-dev
-gpg --keyserver hkp://keyserver.pgp.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-rvm install 2.7.4
-rvm use 2.7.4 --default
+apt-get install -y ruby-full
