50895661f5a17a3c6c8bce47f01a6ba79083063f
Fernando Mejia <66635444+ferantoMSFT@users.noreply.github.com>
2021-09-02 17:06:17 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -21,10 +21,3 @@ cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
-# setup and install ruby
-apt-get install -y grep md5sum sha1sum sha256sum sha512sum tar bzip2 patch gcc wget
-wget -O ruby-install-0.8.2.tar.gz https://github.com/postmodern/ruby-install/archive/v0.8.2.tar.gz
-tar -xzvf ruby-install-0.8.2.tar.gz
-cd ruby-install-0.8.2/
-sudo make install
-ruby-install ruby 2.7.4
