1358fabddcc825fede1bf7a19d5742f33d496cbd
Fernando Mejia <66635444+ferantoMSFT@users.noreply.github.com>
2021-09-02 19:56:01 +0000

    update Implementing github codespaces for inner-loop using github pages, ruby #3

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,7 +7,12 @@ apt-get install -y \
   jq \
   sudo \
   zsh \
-  icu-devtools 
+  icu-devtools
+
+# set-up and install yarn
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+# apt-get update && apt-get install yarn -y
 
 # setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -16,5 +21,8 @@ cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
-# setup and install ruby using rvm 
-apt-get install -y ruby-full
+# setup and install ruby
+apt-get install -y ruby-full build-essential zlib1g-dev
+
+# setup and install jekyll
+gem install jekyll bundler
