e8240c4e7f87e1f2291018b2d3f71b3aed6caf9e
David Rei <david.rei@microsoft.com>
2023-03-15 19:45:35 +0100

    build/fixing-container-and-dependancy (#109)
    Co-authored-by: David Rei <darei@microsoft.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -20,7 +20,3 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-
-# setup and install ruby, jekyll and bundler
-apt-get install -y ruby-full build-essential zlib1g-dev
-gem install jekyll bundler
